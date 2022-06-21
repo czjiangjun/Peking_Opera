eb2a2ce91b780f2a5709ce0559507016b4ef4b12
Jun-Jiang <czjiangjun@gmail.com>
2022-06-22 02:19:36 +0800

    2022-06-21_Script

---
+++ Data/Script.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/bash
+
+var1=`echo $1 | awk -F "." '{print $1}' `
+var2=`echo $1 | awk -F "." '{print $2}' `
+var3="_orig."
+var4="_mid."
+var5="_mod."
+file_back=$var1$var3$var2
+file_mid=$var1$var4$var2
+file_mod=$var1$var5$var2
+echo $1 
+echo $file_back
+
+cp $1 ./$file_back
+
+
+sed 's/\ 【二/\ 【{\\akai\ 二/g' $file_back | sed 's/板】/板}】/g' > $file_mid
+sed 's/\ 【西/\ 【{\\akai\ 西/g' $file_mid | sed 's/眼】/眼}】/g' > $file_mod
